63653de00feb79ecde6f805ec4719afd0c777c3d
Alexander Bolton <alex@hectospark.co.uk>
2019-03-19 21:19:53 +0000

    updated h file

--- DE1_SOC_PONG/DE1SoC_VGA/DE1SoC_VGA.h
+++ DE1_SOC_PONG/DE1SoC_VGA/DE1SoC_VGA.h
@@ -1,6 +1,8 @@
 #ifndef DE1SOC_VGA_H
 #define DE1SOC_VGA_H
 
-void VGA_init();
+void VGA_init(unsigned volatile int VGA);
+void VGA_drawPixel(int x, int y, short colour);
+void VGA_clearScreen(int x, int y, short colour);
 
 #endif

6d650eaf53307e5677aa792ba3b89b2889c56a57
Alexander Bolton <alex@hectospark.co.uk>
2019-03-27 02:20:01 +0000

    Ball created

--- DE1_SOC_PONG/main.c
+++ DE1_SOC_PONG/main.c
@@ -5,7 +5,7 @@ int main(void){
 //VGA TESTCODE
 	int x;
 	Displays_init(0xC8000000,0xC9000000,0xFF200060,0xFF200080);
-	Displays_mode(SOFTWAREOCTOFB);
+	Displays_mode(NOFRAMEBUFFER);
 	pongSprites_init();
 	Displays_clearScreen();
 
@@ -16,7 +16,7 @@ int main(void){
 		Displays_Refresh();
 		pongSprites_renderBall(x, 70,0x0000);
 	}
-	for (x=320-17;x>0+17;x--)
+	for (x=320-17;x>0;x--)
 	{
 		pongSprites_renderBall(x, 70,0xffff);
 		Displays_Refresh();

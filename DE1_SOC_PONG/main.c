01f1f5afe491f572727f5b728ebfdb9f26863a68
Alexander Bolton <alex@hectospark.co.uk>
2019-03-27 02:05:03 +0000

    First attempt
    Slow response, switch to DDR

--- DE1_SOC_PONG/main.c
+++ DE1_SOC_PONG/main.c
@@ -1,38 +1,26 @@
 #include "DE1Soc_VGA/DE1SoC_VGA.h"
 #include "pongDisplay/pongDisplay.h"
-
+#include "pongEngine/pongSprites.h"
 int main(void){
 //VGA TESTCODE
 	int x;
 	Displays_init(0xC8000000,0xC9000000,0xFF200060,0xFF200080);
 	Displays_mode(SOFTWAREOCTOFB);
+	pongSprites_init();
 	Displays_clearScreen();
 
 	while(1){
-	for (x=0;x<320;x++)
+	for (x=0;x<320-17;x++)
 	{
-	  // Draw a straight line in red across the screen centre
-	   Displays_setPixel(x, 59, 0xf800);
-	   //Displays_drawPixel(x, 59, 0x07e0);
-	  // Draw a "diagonal" line in green
-	  if (x<240)
-		  //Displays_drawPixel(x, x, 0xf800);
-		  Displays_setPixel(x, x, 0x07e0);
-
+		pongSprites_renderBall(x, 70,0xffff);
+		Displays_Refresh();
+		pongSprites_renderBall(x, 70,0x0000);
 	}
-	Displays_Refresh();
-	usleep(1000000);
-	for (x=0;x<320;x++)
-		{
-		  // Draw a straight line in red across the screen centre
-		   //Displays_drawPixel(x, 59, 0xf800);
-		   Displays_setPixel(x, 59, 0x07e0);
-		  // Draw a "diagonal" line in green
-		  if (x<240)
-			  Displays_setPixel(x, x, 0x0000);
-			//Displays_drawPixel(x, x, 0x07e0);
-		}
-	Displays_Refresh();
-	usleep(1000000);
+	for (x=320-17;x>0+17;x--)
+	{
+		pongSprites_renderBall(x, 70,0xffff);
+		Displays_Refresh();
+		pongSprites_renderBall(x, 70,0x0000);
+	}
 	}
 }
---
+++ DE1_SOC_PONG/pongEngine/pongSprites.h
@@ -0,0 +1,10 @@
+#ifndef PONGDISPLAY_H
+#define PONGDISPLAY_H
+
+#include "../pongDisplay/pongDisplay.h"
+void pongSprites_init();
+void pongSprites_initBall();
+void pongSprites_renderBall(int x, int y, short colour);
+void pongSprites_renderPaddle(int x, int y, short colour);
+
+#endif

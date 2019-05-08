935f31a0fd4c7e09228eabf04ec6cd9468686f8a
Alex <Alex@hectospark.co.uk>
2019-05-08 21:24:57 +0100

    VGA Driver section complete, added reference to nois DE1-SoC manual

--- DE1_SOC_PONG/DE1SoC_VGA/DE1SoC_VGA.c
+++ DE1_SOC_PONG/DE1SoC_VGA/DE1SoC_VGA.c
@@ -1,10 +1,5 @@
 #include "DE1SoC_VGA.h"
 
-//http://www-ug.eecg.utoronto.ca/msl/nios_devices_SoC/dev_vga.html
-//http://www-ug.eecg.toronto.edu/msl/handouts/DE1-SoC_Computer_Nios.pdf#page=34
-//320x240 pixel resolution
-//volatile short *vga_addr = 0x08000000; //0x08000000
-
 short vga_address = 0x0; 	//0xC8000000
 short vga_charaddr = 0x0; 	//0xC9000000
 volatile int pixel_buffer_start;
@@ -21,13 +16,14 @@ void VGA_init(short vgaPixelBuffer_address, short vgaCharBuffer_address){
 	//Clear VGA
 	VGA_clearScreen();
 }
-
+//This function gets the address of a pixel and sets it to a colour
 void VGA_drawPixel(int x, int y, short colour){
 	//Call address of pixel
+    //Address base + [Pixel Y][Pixel X]0
 	volatile short *vga_addr=(volatile short*)(0xC8000000 + (y<<10) + (x<<1));
 	*vga_addr=colour; //Set pixel to colour
 }
-
+//This function sets all pixels to black
 void VGA_clearScreen(){
 	  int x, y;
 	  for (x = 0; x < 320; x++) {
@@ -37,26 +33,7 @@ void VGA_clearScreen(){
 		}
 	  }
 }
-
-void VGA_drawPixelToHwBuffer(int x, int y, short colour){
-	//Call address of pixel
-	volatile short *vga_addr=(volatile short*)(pixel_buffer_start + (y<<10) + (x<<1));
-	*vga_addr=colour; //Set pixel to colour
-}
-
-void VGA_BufferSwap(){
-	volatile short *vga_addr=(volatile short*)(0xFF203020);
-	volatile int *status=(volatile int*)(0xFF20302C);
-	int stat = 0;
-	*vga_addr= 1;
-	//wait while vertical sync
-	while(stat == 0){
-	if((*status & (1<<0))== 0){
-		stat = 1;
-		}
-	}
-}
-
+//This function sets all pixels to the desired colour
 void VGA_fillColour(short colour){
 	  int x, y;
 	  for (x = 0; x < 320; x++) {

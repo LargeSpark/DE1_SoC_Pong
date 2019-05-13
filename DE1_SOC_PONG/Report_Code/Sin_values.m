a79237cb4a3659e7b4b985e2940140cb2e7749ef
Sam <sam.wilcock0@gmail.com>
2019-05-13 15:46:01 +0100

    Created Report_Code folder for matlab etc

--- DE1_SOC_PONG/Report_Code/Sin_values.m
+++ DE1_SOC_PONG/Report_Code/Sin_values.m
@@ -4,8 +4,8 @@ err_max = 0;
 
 for theta = 0:89
     fprintf(file_handle, '%d, ', round(sind(theta)*10^5));
-    if (abs((sind(0:90)*10^5-(round(sind(0:90)*10^5)))/10^5) > err_max)
-        err_max = (sind(0:90)*10^5-(round(sind(0:90)*10^5)))/10^5;
+    if (abs((sind(theta)*10^5-(round(sind(theta)*10^5)))/10^5) > err_max)
+        err_max = (sind(theta)*10^5-(round(sind(theta)*10^5)))/10^5;
     end
 end
 
@@ -18,3 +18,4 @@ plot(sind(0:90), 'b');
 hold on;
 plot((round(sind(0:90)*10^5))/10^5, 'rx');
 title('Difference, sin and fastsin');
+disp(err_max);
